0be30980f68c7883f594946517529d65d997ab8c
Tim Staats <t.staats@portrix.net>
2018-12-19 11:38:26 +0100

    adding Login Fragment

--- app/src/main/java/com/example/tstaats/mytrees/MainActivity.java
+++ app/src/main/java/com/example/tstaats/mytrees/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.tstaats.mytrees;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate: is called");
+
+        LoginFragment loginFragment = new LoginFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, loginFragment);
+        transaction.commit();
+        
     }
 }
--- app/src/main/java/com/example/tstaats/mytrees/Tree.java
+++ app/src/main/java/com/example/tstaats/mytrees/Tree.java
@@ -1,4 +1,6 @@
 package com.example.tstaats.mytrees;
 
 public class Tree {
+
+
 }
