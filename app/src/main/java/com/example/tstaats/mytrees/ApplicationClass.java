7b68de45fbf815c5f9d2e29ef54e949e6c469750
Tim Staats <t.staats@portrix.net>
2018-12-19 11:06:22 +0100

    initial commit

---
+++ app/src/main/java/com/example/tstaats/mytrees/ApplicationClass.java
@@ -0,0 +1,31 @@
+package com.example.tstaats.mytrees;
+
+
+import android.app.Application;
+
+import com.backendless.Backendless;
+import com.backendless.BackendlessUser;
+
+import java.util.List;
+
+// very first class that the application is running, even before the MainActivity
+public class ApplicationClass extends Application {
+
+    public static final String APPLICATION_ID = "7E4037CC-1A42-F063-FF97-4482E7BA9000";
+    public static final String API_KEY = "913ACE63-EFF8-1DFA-FF1E-D502D1163300";
+    public static final String SERVER_URL = "https://api.backendless.com";
+
+    public static BackendlessUser user;
+    public static List<Tree> treeList;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Backendless.setUrl(SERVER_URL);
+        Backendless.initApp(getApplicationContext(),
+                APPLICATION_ID,
+                API_KEY);
+
+    }
+}
---
+++ app/src/main/java/com/example/tstaats/mytrees/Tree.java
@@ -0,0 +1,4 @@
+package com.example.tstaats.mytrees;
+
+public class Tree {
+}
