59b7fbaf90e81a2d202c7d49e807dcc833ceb3e3
Tim Staats <t.staats@portrix.net>
2018-12-20 13:46:03 +0100

    rearange classes for backendless saving multiple treestates for one tree-entry

---
+++ app/src/main/java/com/example/tstaats/mytrees/TreeState.java
@@ -0,0 +1,41 @@
+package com.example.tstaats.mytrees;
+
+public class TreeState {
+
+    private String imgUrl;
+    private String created;
+    private String updated;
+    private String stateDescription;
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public String getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(String updated) {
+        this.updated = updated;
+    }
+
+    public String getStateDescription() {
+        return stateDescription;
+    }
+
+    public void setStateDescription(String stateDescription) {
+        this.stateDescription = stateDescription;
+    }
+}
